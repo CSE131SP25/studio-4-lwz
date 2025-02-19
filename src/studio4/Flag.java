cecfb82cc2f43f83d9c4be9b1b127c6eb118e535
ninazhang <ninazhang@localhost>
2025-02-19 14:15:57 -0600

    finished

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,23 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		int redOne = 255;
+		int greenOne = 60;
+		int blueOne = 184;
+		Color pink = new Color(redOne, greenOne, blueOne);
+		
+		StdDraw.point(0.5, 0.5);
+		StdDraw.setPenColor(pink);
+		StdDraw.filledRectangle(0.5, 0.5, 0.3, 0.2);
+		
+		int redTwo = 255;
+		int greenTwo = 246;
+		int blueTwo = 158;
+		Color yellow = new Color(redTwo, greenTwo, blueTwo);
+		
+		StdDraw.point(0.5, 0.5);
+		StdDraw.setPenColor(yellow);
+		StdDraw.filledRectangle(0.5, 0.5, 0.3, 0.05);
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,25 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+		Color color = new Color(red, green, blue);
+		
+		StdDraw.point(parameterOne,parameterTwo);
+		StdDraw.setPenColor(color);
+		if(filled == true) {
+			StdDraw.filledRectangle(parameterOne, parameterTwo, halfWidth, halfHeight);
+		} else {
+			StdDraw.rectangle(parameterOne, parameterTwo, halfWidth, halfHeight);
+		}
 		
 	}
 }
